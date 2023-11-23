905fedaa091cbeb9ffb03e29a4638176b804ef0d
Enes Yetgin <113235626+enyetgin@users.noreply.github.com>
2023-11-23 23:40:51 +0300

    Delete tobeto/java.spring directory

--- tobeto/java.spring/src/main/java/com/tobeto/java/spring/repositories/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tobeto.java.spring.repositories;
-
-import com.tobeto.java.spring.entities.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-
-}
--- tobeto/java.spring/src/main/java/com/tobeto/java/spring/repositories/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tobeto.java.spring.repositories;
-
-import com.tobeto.java.spring.entities.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
-
-}
